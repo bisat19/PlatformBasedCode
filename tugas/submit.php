340013dc0cc11760f042b7d646d0c630a2e414ab
Abi Satya Deville <118191553+bisat19@users.noreply.github.com>
2024-09-09 10:18:48 +0700

    Update submit.php

--- tugas/submit.php
+++ tugas/submit.php
@@ -1,5 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $birthPlace = htmlspecialchars($_POST['birthPlace']);
@@ -9,9 +10,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $address = htmlspecialchars($_POST['address']);
     $postalCode = htmlspecialchars($_POST['postalCode']);
     $country = htmlspecialchars($_POST['country']);
+    $departemen = htmlspecialchars($_POST['departemen']);
     $interest = isset($_POST['interest']) ? implode(", ", $_POST['interest']) : "None";
 
+    echo "Data User";
     echo "<table border='1'>";
+    echo "<tr><th>Username</th><td>$username</td></tr>";
     echo "<tr><th>Nama Lengkap</th><td>$name</td></tr>";
     echo "<tr><th>Alamat Email</th><td>$email</td></tr>";
     echo "<tr><th>Tempat Lahir</th><td>$birthPlace</td></tr>";
@@ -19,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<tr><th>Alamat Rumah</th><td>$address</td></tr>";
     echo "<tr><th>Kode Pos</th><td>$postalCode</td></tr>";
     echo "<tr><th>Negara</th><td>$country</td></tr>";
+    echo "<tr><th>Departemen</th><td>$departemen</td></tr>";
     echo "<tr><th>Peminatan</th><td>$interest</td></tr>";
     echo "</table>";
 }
